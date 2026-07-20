c0f71f97545ab6033ac05f4e2d8f0ea5a1e31d2c
siri251 <sireesha.chelluri2001@gmail.com>
2026-07-20 12:45:02 +0530

    added before line

--- app.js
+++ app.js
@@ -9,10 +9,11 @@ const cookieParser = require("cookie-parser");
 
 app.use(express.json())
 app.use(cors())
+app.use(cookieParser());  
 app.use('/api/auth', authRouter)
 app.use('/api/projects', projectRouter)
 app.use("/api/tasks", taskRouter)
-app.use(cookieParser());   
+ 
 
 app.get('/', (req, res) => {
     res.json({ message: 'API is running' })
