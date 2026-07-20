c922520d840c7fc8a15f8242a74788d1210f397f
siri251 <sireesha.chelluri2001@gmail.com>
2026-07-20 12:42:02 +0530

    added cookie parser

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ const connectDb = require("./src/config/db");
 const authRouter = require('./src/routes/auth')
 const projectRouter = require("./src/routes/projects");
 const taskRouter = require("./src/routes/tasks");
+const cookieParser = require("cookie-parser");
 
 app.use(express.json())
 app.use(cors())
 app.use('/api/auth', authRouter)
 app.use('/api/projects', projectRouter)
 app.use("/api/tasks", taskRouter)
+app.use(cookieParser());   
 
 app.get('/', (req, res) => {
     res.json({ message: 'API is running' })
