5f25813d012a30718aa8b41d214bf63b94390bf7
siri251 <sireesha.chelluri2001@gmail.com>
2026-07-20 18:21:51 +0530

    worked on cors

--- app.js
+++ app.js
@@ -8,6 +8,21 @@ const taskRouter = require("./src/routes/tasks");
 const cookieParser = require("cookie-parser");
 
 app.use(express.json())
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://your-frontend-domain.com"
+];
+
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    credentials: true
+}));
 app.use(cors())
 app.use(cookieParser());  
 app.use('/api/auth', authRouter)
